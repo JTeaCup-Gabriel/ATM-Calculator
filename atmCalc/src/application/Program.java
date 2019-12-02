2f1fd6f6b86ad5c82d255815c8bed1413764c447
Gabriel Ferreira <GF_JTeaCup_BR@outlook.com>
2019-12-02 16:30:08 -0300

    Main inputs, outputs and calc system created

--- atmCalc/src/application/Program.java
+++ atmCalc/src/application/Program.java
@@ -4,6 +4,7 @@
 package application;
 
 import java.io.IOException;
+import java.text.DecimalFormat;
 import java.util.Locale;
 import java.util.Scanner;
 
@@ -17,10 +18,28 @@ public class Program {
 		// ------------------------------------------------------------------- >
 		Locale.setDefault(Locale.US);
 		Scanner sc = new Scanner(System.in);
-		double N = sc.nextDouble();
+		DecimalFormat dff = new DecimalFormat("0.00");
+		int N = 0;
+		int cem = 0, cinq = 0, vint = 0, dez = 0, cinc = 0, dois = 0, hum = 0;
 		// ------------------------------------------------------------------- >
-		System.out.println(N);
+		N = sc.nextInt();
 		// ------------------------------------------------------------------- >
+		cem = N / 100;
+		N %= 100;
+		// ------------ >
+		cinq = N / 50;
+		N %= 50;
+
+		// ------------------------------------------------------------------- >
+		System.out.println(dff.format(N));
+		// ------------------------------------------------------------------- >
+		System.out.println(cem);
+		System.out.println(cinq);
+		System.out.println(vint);
+		System.out.println(dez);
+		System.out.println(cinc);
+		System.out.println(dois);
+		System.out.println(hum);
 		sc.close();
 	}
 
