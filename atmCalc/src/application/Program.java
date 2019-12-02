4bca01a89f88cdf189538055871fbd6a380e6d3e
Gabriel Ferreira <GF_JTeaCup_BR@outlook.com>
2019-12-02 16:15:11 -0300

    Algorithm starts

--- atmCalc/src/application/Program.java
+++ atmCalc/src/application/Program.java
@@ -3,18 +3,25 @@
  */
 package application;
 
+import java.io.IOException;
+import java.util.Locale;
+import java.util.Scanner;
+
 /**
- * @author Gabriel
- * Basic algorithm to calculate the quantity of notes
+ * @author Gabriel Basic algorithm to calculate the quantity of notes
  *
  */
 public class Program {
 
-	public static void main(String[] args) {
-		
-		System.out.println("Teste");
-		
-
+	public static void main(String[] args) throws IOException {
+		// ------------------------------------------------------------------- >
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double N = sc.nextDouble();
+		// ------------------------------------------------------------------- >
+		System.out.println(N);
+		// ------------------------------------------------------------------- >
+		sc.close();
 	}
 
 }
